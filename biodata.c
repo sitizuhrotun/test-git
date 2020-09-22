b5cb32130e0b7bbc208560dd4a11068ae50f72fd
sitizuhrotun <filezuhro@gmail.com>
2020-09-22 10:12:54 +0700

    first commit

---
+++ biodata.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+	
+	printf ("Nama : Siti Zuhrotun Nafi'atur Rohmah\n");
+	printf ("NPM : 19081010067\n");
+	printf ("Prodi : Informatika\n");
+	printf ("Angkatan : 2019\n");
+	printf ("Fakultas : Ilmu Komputer\n");
+	printf ("Alamat : Tuban\n");	
+
+	return 0;
+}
